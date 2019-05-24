f59368bd6a7b156a847bfd376374b6b489307f68
Franck Garcon <franck@donkeycode.com>
2019-05-24 12:55:19 +0200

    menu.color

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+// Gestion de l'affichage du menu avec un effet "mouseover"
 $ (document).ready (function () {
     $ (document).delegate ('.open', 'mouseover', function (event) {
         $ (this).addClass ('oppenned');
